b6500b4a22d2475bf4ecde236669e258792ef66f
Tomas Sanni <tomas.sanni7@gmail.com>
2024-06-17 07:14:44 -0500

    add sounds

---
+++ Xylophone/NoteButton.swift
@@ -0,0 +1,20 @@
+//
+//  NoteButton.swift
+//  Xylophone
+//
+//  Created by Tomas Sanni on 6/17/24.
+//
+
+import UIKit
+
+class NoteButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
