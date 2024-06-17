d4d50384802b39c67ddc8c14b5b8bad21ab28e10
Tomas Sanni <tomas.sanni7@gmail.com>
2024-06-17 07:15:11 -0500

    add sounds

--- Xylophone/NoteButton.swift
+++ Xylophone/NoteButton.swift
@@ -6,15 +6,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class NoteButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var player: AVAudioPlayer!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        addTarget(self, action: #selector(handleButton), for: .touchUpInside)
+        configure()
+    }
+    
+    @objc func handleButton() {
+        print("Note:", titleLabel?.text ?? "A")
+        let url = Bundle.main.url(forResource: titleLabel?.text, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            heightAnchor.constraint(equalToConstant: 110)
+        ])
+    }
+    
+//    func set(backgroundColor: UIColor, title: String) {
+//        self.backgroundColor = backgroundColor
+//        setTitle(title, for: .normal)
+//    }
+    
 }
--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let CButton = NoteButton(backgroundColor: .systemRed, title: "C")
+    let DButton = NoteButton(backgroundColor: .systemOrange, title: "D")
+    let EButton = NoteButton(backgroundColor: .systemYellow, title: "E")
+    let FButton = NoteButton(backgroundColor: .systemGreen, title: "F")
+    let GButton = NoteButton(backgroundColor: .purple, title: "G")
+    let AButton = NoteButton(backgroundColor: .systemBlue, title: "A")
+    let BButton = NoteButton(backgroundColor: .systemPurple, title: "B")
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        configureButtons()
     }
 
+    func configureButtons() {
+        view.addSubview(CButton)
+        view.addSubview(DButton)
+        view.addSubview(EButton)
+        view.addSubview(FButton)
+        view.addSubview(GButton)
+        view.addSubview(AButton)
+        view.addSubview(BButton)
+
+
+        NSLayoutConstraint.activate([
+            CButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            CButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            CButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            DButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            DButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            DButton.topAnchor.constraint(equalTo: CButton.bottomAnchor, constant: 10),
+            
+            EButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            EButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            EButton.topAnchor.constraint(equalTo: DButton.bottomAnchor, constant: 10),
+            
+            FButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            FButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            FButton.topAnchor.constraint(equalTo: EButton.bottomAnchor, constant: 10),
+            
+            GButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            GButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            GButton.topAnchor.constraint(equalTo: FButton.bottomAnchor, constant: 10),
+            
+            AButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 35),
+            AButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -35),
+            AButton.topAnchor.constraint(equalTo: GButton.bottomAnchor, constant: 10),
+            
+            BButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            BButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            BButton.topAnchor.constraint(equalTo: AButton.bottomAnchor, constant: 10),
+            
+        ])
+        
+    }
 
 }
 
+
+
+#Preview {
+    ViewController()
+}
